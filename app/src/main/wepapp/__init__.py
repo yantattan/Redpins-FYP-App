1c240f375ba520ee9df3237349991616cda831b3
uudhayaa <101234906+uudhayaa@users.noreply.github.com>
2022-04-06 13:54:09 +0800

    Update __init__.py

--- app/src/main/wepapp/__init__.py
+++ app/src/main/wepapp/__init__.py
@@ -1,6 +1,4 @@
-from time import strftime
 from urllib.error import HTTPError
-from cv2 import split
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 from apscheduler.scheduler import Scheduler
 from io import BytesIO
@@ -14,7 +12,7 @@ from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 # Email libraries
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -33,7 +31,7 @@ cron = Scheduler(daemon=True)
 cron.start()
 CORS(app)
 app.secret_key = "redp1n5Buffer"
-apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjg1NDQsInVzZXJfaWQiOjg1NDQsImVtYWlsIjoieWFudGF0dGFuNzIxQGdtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC9vbTIuZGZlLm9uZW1hcC5zZ1wvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTY0OTAzNzU3NywiZXhwIjoxNjQ5NDY5NTc3LCJuYmYiOjE2NDkwMzc1NzcsImp0aSI6IjczZWM5ODM4ZmZjNmJkM2I3YWViNzEzMjNjZjM2YmVjIn0.baBuTC8AbXu8kPKWvTumTgkpqDpbjVCfAhcm7kaO9-Y"
+apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjg1NDQsInVzZXJfaWQiOjg1NDQsImVtYWlsIjoieWFudGF0dGFuNzIxQGdtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC9vbTIuZGZlLm9uZW1hcC5zZ1wvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTY0ODQ0NTM1NCwiZXhwIjoxNjQ4ODc3MzU0LCJuYmYiOjE2NDg0NDUzNTQsImp0aSI6IjBkYWIzYTIyMDIwMmYxZDA5YzJhYmMxYjEyZjk0M2RhIn0.ygc_daPpwuKKaMqR1MHLVVKBCOtO9cYtGdAnLALGlGI"
 
 # Init all controllers
 userCon = Users.UserCon()
@@ -41,6 +39,8 @@ trackedPlacesCon = TrackedPlaces.TrackedPlacesCon()
 signedPlaceCon = SignedPlaces.SignedPlacesCon()
 machineLearningReportCon = MachineLearningReports.MachineLearningReportCon()
 placesBonusCodesCon = PlacesBonusCodes.PlacesBonusCodesCon()
+
+
 # preferencesCon = Preferences.PreferencesCon()
 # userRewardsCon = UserPoints.UserPointsCon()
 
@@ -92,8 +92,9 @@ def register():
 
     if request.method == 'POST':
         if register_form.validate():
-            userModel = User(register_form.username.data, register_form.email.data, "User", register_form.dateOfBirth.data, 
-                            register_form.contact.data, register_form.password.data, 0, 0, "Bronze")
+            userModel = User(register_form.username.data, register_form.email.data, "User",
+                             register_form.dateOfBirth.data,
+                             register_form.contact.data, register_form.password.data, 0, "Bronze")
             registerResponse = userCon.Register(userModel)
             print(registerResponse)
             if registerResponse.get("error"):
@@ -112,13 +113,14 @@ def forgetPassword():
 
     if request.method == 'POST' and forget_password_form.validate():
         # Generate random string of characters
-        newPassword = "".join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(12))
+        newPassword = "".join(
+            random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(12))
 
         # Reset password
         result = userCon.ChangePassword(session['current_user']['userId'], newPassword)
         if result.get("success"):
             # Create email to send new password
-            mailFrom = "redpinsbuffer@gmail.com" 
+            mailFrom = "redpinsbuffer@gmail.com"
             mailTo = forget_password_form.email.data
             msg = MIMEMultipart('alternative')
             msg['Subject'] = "Change of password"
@@ -140,7 +142,7 @@ def forgetPassword():
             mail.quit()
 
             return redirect("/login")
-            
+
         return render_template("accounts/forgetPassword.html", form=forget_password_form, error="An error occurred")
 
     return render_template("accounts/forgetPassword.html", form=forget_password_form)
@@ -164,7 +166,7 @@ def pref1():
 
 
 def getpoints(isBonus):
-    Utier = {"Bronze": 1, "Silver": 1.1, "Gold": 1.2, "Diamond": 1.5}
+    Utier = {"Bronze": 1, "Silver": 1.1, "Gold": 1.3, "Diamond": 1.5}
     Uid = session["current_user"]["userId"]
     result = {"shopName": "MARINA BAY SANDS", "address": "1 BAYFRONT AVENUE MARINA BAY SANDS SINGAPORE 018971"}
     result2 = signedPlaceCon.GetShopInfo(result["address"])
@@ -172,11 +174,13 @@ def getpoints(isBonus):
     if isBonus:
         points = result2.getPoints()
     result3 = userCon.GetUserPointsInfo(Uid)
+    result4 = userCon.tierPoints()
     uTierMultiplier = Utier.get(result3.getTier())
     uPoints = result3.getPoints()
     total_pts_earned = points * uTierMultiplier
     new_upoints = total_pts_earned + uPoints
-    userCon.SetPoints(Uid, new_upoints)
+    new_tierPoints = total_pts_earned + result4
+    userCon.SetPoints(Uid, new_upoints, tierPoints)
 
     def UpdateTier():
         OldRank = result3.getTier()
@@ -198,12 +202,23 @@ def getpoints(isBonus):
 def scanQR():
     return render_template("qrSites/qrScanner.html")
 
+
 @app.route("/qrCode/claim-bonus/<string:id>")
 def qrCodeClaimBonus(id):
     print("Test")
 
+
 @app.route("qrCode/use-points/<string:id>")
 def qrCodeUsePoints(id):
+    Total_price = " "
+    Uid = session["current_user"]["userId"]
+    resultDic = {"shopName": "MARINA BAY SANDS", "address": "1 BAYFRONT AVENUE MARINA BAY SANDS SINGAPORE 018971"}
+    result = userCon.GetUserPointsInfo(Uid)
+    result2 = signedPlacesCon.GetShopInfo(resultDic["address"])
+    discount = result2.getDiscount()
+    uPoints = result.getPoints()
+    new_uPoints = uPOints - Total_price*(1 - discount)
+    userCon.SetPoints(Uid, new_upoints, tierPoints  )
     print("Test")
 
 
@@ -213,26 +228,29 @@ def viewSignedPlaces():
     validateAdmin()
     return render_template("admin/viewSignedPlaces.html")
 
+
 @app.route("/admin/signedPlaces/create", methods=['GET', 'POST'])
 def adminCreatePlace():
     validateAdmin()
     signedPlaceForm = SignedPlaceForm(request.form)
     if request.method == 'POST':
         if signedPlaceForm.validate():
-            signedPlace = SignedPlaces.SignedPlace(None, signedPlaceForm.address.data, signedPlaceForm.unitNo.data, 
-                        signedPlaceForm.shopName.data, signedPlaceForm.organization.data, signedPlaceForm.points.data, 
-                        signedPlaceForm.checkpoint.data, signedPlaceForm.discount.data)
+            signedPlace = SignedPlaces.SignedPlace(None, signedPlaceForm.address.data, signedPlaceForm.unitNo.data,
+                                                   signedPlaceForm.shopName.data, signedPlaceForm.organization.data,
+                                                   signedPlaceForm.points.data,
+                                                   signedPlaceForm.checkpoint.data, signedPlaceForm.discount.data)
             response = signedPlaceCon.CreateEntry(signedPlace)
 
             if response.get("success"):
                 return redirect("/admin/signedPlaces")
             else:
                 return render_template("admin/createSignedPlace.html", form=signedPlaceForm, error=response["error"])
-        
+
         return render_template("admin/createSignedPlace.html", form=signedPlaceForm, error="Invalid inputs entered")
 
     else:
         return render_template("admin/createSignedPlace.html", form=signedPlaceForm)
+
 
 @app.route("/admin/signedPlaces/update/<string:id>", methods=['GET', 'POST'])
 def adminUpdatePlace(id):
@@ -240,9 +258,10 @@ def adminUpdatePlace(id):
     signedPlaceForm = SignedPlaceForm(request.form)
     if request.method == 'POST':
         if signedPlaceForm.validate():
-            signedPlace = SignedPlaces.SignedPlace(id, signedPlaceForm.address.data, signedPlaceForm.unitNo.data, 
-                            signedPlaceForm.shopName.data, signedPlaceForm.organization.data, signedPlaceForm.points.data, 
-                            signedPlaceForm.checkpoint.data, signedPlaceForm.discount.data)
+            signedPlace = SignedPlaces.SignedPlace(id, signedPlaceForm.address.data, signedPlaceForm.unitNo.data,
+                                                   signedPlaceForm.shopName.data, signedPlaceForm.organization.data,
+                                                   signedPlaceForm.points.data,
+                                                   signedPlaceForm.checkpoint.data, signedPlaceForm.discount.data)
             response = signedPlaceCon.UpdateEntry(signedPlace)
             print(response)
 
@@ -250,7 +269,7 @@ def adminUpdatePlace(id):
                 return redirect("/admin/signedPlaces")
             else:
                 return render_template("admin/editSignedPlace.html", form=signedPlaceForm, error=response["error"])
-        
+
         return render_template("admin/editSignedPlace.html", form=signedPlaceForm, error="Invalid inputs entered")
 
     else:
@@ -268,17 +287,20 @@ def adminUpdatePlace(id):
 
         return redirect("/admin/signedPlaces")
 
+
 @app.route("/admin/signedPlaces/delete/<string:id>")
 def adminDeletePlace(id):
     validateAdmin()
     signedPlaceCon.DeleteEntry(id)
     return redirect("/admin/signedPlaces")
 
+
 @app.route("/admin/signedPlaces/registerPurchase/<string:id>")
 def adminRegisterPurchase(id):
     validatePlaceAdmin()
 
     placesBonusCodesCon.GenerateCode(id)
+
 
 # AJAX CALLS
 # Reward points -- Assign rewards point (Udhaya)
@@ -287,7 +309,7 @@ def recommendDestination():
     averageSpeeds = {
         "walk": 4,
         "drive": 60,
-        "pt": (80*0.45 + 50*0.45 + 4*0.1),
+        "pt": (80 * 0.45 + 50 * 0.45 + 4 * 0.1),
         "cycle": 18
     }
 
@@ -306,22 +328,22 @@ def recommendDestination():
                 if estTime <= timeAllowance:
                     # 2nd stage shortlisting - Determined by the fastest route (actual timing needed)
                     currentTime = datetime.now()
-                    routeResults = json.loads("https://developers.onemap.sg/privateapi/routingsvc/route?start=${},${}&end=${},${}" \
-                                    "&routeType=${}&token=${}&date=${}&time=${}" \
-                                    "&mode=TRANSIT".format(startCoords.latitude, startCoords.longitutde, placeLatlng[0], 
-                                                        placeLatlng[1], transportMode, apiKey, currentTime.strftime("%Y-%m-%d"), 
-                                                        currentTime.strftime("%H:%M:%S")) )
+                    routeResults = json.loads(
+                        "https://developers.onemap.sg/privateapi/routingsvc/route?start=${},${}&end=${},${}" \
+                        "&routeType=${}&token=${}&date=${}&time=${}" \
+                        "&mode=TRANSIT".format(startCoords.latitude, startCoords.longitutde, placeLatlng[0],
+                                               placeLatlng[1], transportMode, apiKey, currentTime.strftime("%Y-%m-%d"),
+                                               currentTime.strftime("%H:%M:%S")))
                     estTime = 0.75
                     if transportMode == "pt":
                         estTime += routeResults["plan"]["itineraries"][0]["duration"] / 3600
                     else:
                         estTime += routeResults["route_summary"]["total_time"] / 3600
-                    
+
                     if estTime <= timeAllowance:
                         finalList.append(place)
-            
-            return finalList
 
+            return finalList
 
         startCoords = request.form.get("startCoords")
         timeAllowance = request.form.get("timeAllowance")
@@ -334,34 +356,40 @@ def recommendDestination():
             # Shortlisted after calculating distance
             shortlistedPlaces = ShortlistByDistance(placesList["SrchResults"], transportMode)
 
-    
+
 # Reward points -- Assign rewards point (Udhaya)
 @app.route("/funcs/reached-place/", methods=['GET', 'POST'])
 def reachedPlace():
     # Placeholder returned data
     getpoints(False)
 
+
 def trackPlaces(places, storeMean):
     # Track down the destinations for future recommendation algorithm
     for place in places:
         trackedPlacesCon.SetInfo(TrackedPlace(session["current_user"]["user_id"], place["address"], storeMean))
+
 
 @app.route("/funcs/post-places/", methods=['POST'])
 def recommendPlace():
     finalResult = {}
     trackPlaces(request.form.get("destinations"), request.form.get("storeMean"))
 
-    return json.dumps(finalResult) 
+    return json.dumps(finalResult)
+
 
 @app.route("/funcs/mark-tracked/", methods=['POST'])
 def markTracked():
     trackPlaces(request.form.get("places"), request.form.get("storeMean"))
 
+
 @app.route("/funcs/admin/table_getSignedPlaces")
 def tableGetSignedPlaces():
     args = request.args
-    resultDict = signedPlaceCon.ViewListOfPlaces(args.get("search"), args.get("sort"), args.get("order"), args.get("limit"), args.get("offset"))
+    resultDict = signedPlaceCon.ViewListOfPlaces(args.get("search"), args.get("sort"), args.get("order"),
+                                                 args.get("limit"), args.get("offset"))
     return json.dumps(resultDict)
+
 
 @app.route("/funcs/generate-arrival-qrcode", methods=['POST'])
 def genQRCode():
@@ -375,9 +403,10 @@ def genQRCode():
     response = send_file(buffer, mimetype='image/png')
     return response
 
+
 @app.route("/funcs/use-points", methods=['POST'])
 def usePoints():
-    return 
+    return
 
 
 # Scheduled tasks to run every week - Web scrap, training of data models
@@ -389,12 +418,12 @@ def scheduledJobs():
         for column_name in data.columns:
             if data[column_name].dtype == object:
                 data[column_name] = le.fit_transform(data[column_name])
-            
-        return data 
+
+        return data
 
     def trainGlobalUserPreferenceModel():
         print("I am training the model")
-        path = userCon.ExportGlobalUserPreferenceCSV() # Supply the new data in db to the csv files
+        path = userCon.ExportGlobalUserPreferenceCSV()  # Supply the new data in db to the csv files
         data = encodeColumns(pandas.read_csv(path))
 
         inp = data.drop(columns=['Preference'])
@@ -415,14 +444,17 @@ def scheduledJobs():
 
         # Saving the model and accuracy results
         model.fit(inp, oup)
-        joblib.dump(model, "csv/dbcsv/global-users-preferences.joblib") # Saves the newly trained model as a file
-        machineLearningReportCon.SetData(MachineLearningReport("GlobalUserPreferences", "Accuracy", meanAccuracyScore)) # Save the accuracy score to db
+        joblib.dump(model, "csv/dbcsv/global-users-preferences.joblib")  # Saves the newly trained model as a file
+        machineLearningReportCon.SetData(MachineLearningReport("GlobalUserPreferences", "Accuracy",
+                                                               meanAccuracyScore))  # Save the accuracy score to db
 
     def webScrap():
         print("I am web scrapping")
-    
+
     trainGlobalUserPreferenceModel()
     webScrap()
+
+
 # Error pages handling
 # @app.errorhandler(403)
 # def forbidden_error(error):
